6a276fefa17bdeabe86e8062f436e06a94390723
Rheno001 <renfredreuben@gmail.com>
2024-12-02 00:40:10 +0100

    catering page started

--- src/pages/catering/index.jsx
+++ src/pages/catering/index.jsx
@@ -1,7 +1,44 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import catering from '../../assets/catering.png'
+import vector from '../../assets/vector.png'
+
 const Catering = () => {
-    return (<>
-        <h1>Catering</h1>
-    </>);
+    return (
+        <div className="menu-container bg-primary">
+            <motion.img 
+                src={catering} 
+                alt="Catering Image" 
+                className="menu-logo px-[65px] pt-20"
+                initial={{ y: -100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                    type: "spring",
+                    bounce: 0.7,
+                    duration: 2.5
+                }}
+            />
+            <section>
+                <div className='flex flex-col items-center justify-center mt-20'>
+                   <p className='text-accent text-xl font-[600] text-[70px] leading-[84px] font-mont align-middle'>Our Catering Services</p>
+                   <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
+               </div>
+               <div className='carousel'></div>
+               <div className='flex items-center justify-evenly px-[80px]'>
+                <span className='text-center p-[50px]'>
+                    <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Full Pan Party</h2>
+                    <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
+                    <p className='text-accent font-[600] text-[18px] leading-[27px]'>$170,00</p>
+                </span>
+                <span className='text-center p-[50px]'>
+                    <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Small Pan Snack</h2>
+                    <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken garnished with onion & cucumber.</h3>
+                    <p className='text-accent font-[600] text-[18px] leading-[27px]'>$300,00</p>
+                </span>
+               </div>
+            </section>
+        </div>
+    );
 }
 
 export default Catering;
--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import logo from '../../assets/logo.png'
 import vector from '../../assets/vector.png'
 import frame120 from '../../assets/Frame120.png'
+import burger from '../../assets/burger.png'
+import salad from '../../assets/two.png'
 
 const Menu = () => {
   return (
     <div className="menu-container bg-primary">
-      <img src={logo} alt="Restaurant Logo" className="menu-logo" />
+      <motion.img 
+        src={logo} 
+        alt="Restaurant Logo" 
+        className="menu-logo"
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          type: "spring",
+          bounce: 0.7,
+          duration: 2.5
+        }}
+      />
       <section className="hero-section bg-primary py-[70px] px-[80px] gap-[28px]">
         <div className='flex flex-col items-center justify-center gap-[10px]'>
           <p className='text-accent text-xl font-[600] text-[50px] leading-[84px] font-mont align-middle'>Suya</p>
@@ -15,17 +29,17 @@ const Menu = () => {
         <div className='flex items-center gap-[35px] pt-[50px]'>
            <img src={frame120} alt="Frame 120" className='w-[720px] h-[695px]'/>
            <div className='text font-mont'>
-            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$20,00</p>
             </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Chillin' Chicken Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$30,00</p>
             </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Wingin' it Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken wings garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
@@ -41,19 +55,19 @@ const Menu = () => {
           <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
         </div>
         <div className='flex items-center gap-[35px] pt-[50px]'>
-           <img src={frame120} alt="Frame 120" className='w-[720px] h-[695px]'/>
+           <img src={salad} alt="Frame 120" className='w-[720px] h-[695px]'/>
            <div className='text font-mont'>
-            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$20,00</p>
             </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Chillin' Chicken Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$30,00</p>
             </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Wingin' it Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken wings garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
@@ -69,19 +83,19 @@ const Menu = () => {
           <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
         </div>
         <div className='flex items-center gap-[35px] pt-[50px]'>
-           <img src={frame120} alt="Frame 120" className='w-[720px] h-[695px]'/>
+           <img src={burger} alt="Frame 120" className='w-[720px] h-[695px]'/>
            <div className='text font-mont'>
-            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$20,00</p>
             </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Chillin' Chicken Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$30,00</p>
             </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Wingin' it Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken wings garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
@@ -97,19 +111,19 @@ const Menu = () => {
           <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
         </div>
         <div className='flex items-center gap-[35px] pt-[50px]'>
-           <img src={frame120} alt="Frame 120" className='w-[720px] h-[695px]'/>
+           <img src={burger} alt="Frame 120" className='w-[720px] h-[695px]'/>
            <div className='text font-mont'>
-            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$20,00</p>
             </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Chillin' Chicken Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$30,00</p>
             </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-accent'>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Wingin' it Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken wings garnished with onion & cucumber.</h3>
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
