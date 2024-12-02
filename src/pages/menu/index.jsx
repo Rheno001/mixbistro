fb3b06d617c69ed22fd629f1b33840b4165ba50f
Rheno001 <renfredreuben@gmail.com>
2024-12-02 06:53:56 +0100

    started working on menu motion

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { useInView } from "react-intersection-observer";
 import logo from '../../assets/logo.png'
 import vector from '../../assets/vector.png'
 import frame120 from '../../assets/Frame120.png'
@@ -7,6 +8,10 @@ import burger from '../../assets/burger.png'
 import salad from '../../assets/two.png'
 
 const Menu = () => {
+  const { ref: ref3, inView: inView3 } = useInView({
+    triggerOnce: false,
+    threshold: 0.5,
+});
   return (
     <div className="menu-container bg-primary">
       <motion.img 
@@ -22,10 +27,20 @@ const Menu = () => {
         }}
       />
       <section className="hero-section bg-primary py-[70px] px-[80px] gap-[28px]">
-        <div className='flex flex-col items-center justify-center gap-[10px]'>
-          <p className='text-accent text-xl font-[600] text-[50px] leading-[84px] font-mont align-middle'>Suya</p>
-          <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
-        </div>
+        <motion.div
+                ref={ref3}
+                initial={{ opacity: 0, y: -200 }}
+                animate={{
+                    opacity: inView3 ? 1 : 0,
+                    y: inView3 ? 0 : -200,
+                }}
+                transition={{ duration: 0.5 }}
+                className='flex justify-center items-center px-[74px]'>
+                <div className='relative flex flex-col items-center'>
+                 <p className='text-accent text-xl font-[600] text-[50px] leading-[84px] font-mont align-middle'>Groovy Green Salad</p>
+                 <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
+                </div>
+        </motion.div>
         <div className='flex items-center gap-[35px] pt-[50px]'>
            <img src={frame120} alt="Frame 120" className='w-[720px] h-[695px]'/>
            <div className='text font-mont'>
