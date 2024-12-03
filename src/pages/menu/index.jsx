f4ab434d67b696cbf17e44ebf4930735f6691246
Rheno001 <renfredreuben@gmail.com>
2024-12-03 12:03:26 +0100

    menu motion done

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -7,10 +7,38 @@ import burger from '../../assets/burger.png'
 import salad from '../../assets/two.png'
 
 const Menu = () => {
-  const { ref: ref3, inView: inView3 } = useInView({
+   const { ref: ref1, inView: inView1 } = useInView({
+     triggerOnce: false,
+     threshold: 0.5,
+   });
+   const { ref: ref2, inView: inView2 } = useInView({
+     triggerOnce: false,
+     threshold: 0.5,
+   });
+   const { ref: ref3, inView: inView3 } = useInView({
+     triggerOnce: false,
+     threshold: 0.5,
+  });
+  const { ref: ref4, inView: inView4 } = useInView({
+     triggerOnce: false,
+     threshold: 0.5,
+  });
+  const { ref: ref5, inView: inView5 } = useInView({
     triggerOnce: false,
     threshold: 0.5,
-});
+  });
+  const { ref: ref6, inView: inView6 } = useInView({
+    triggerOnce: false,
+    threshold: 0.5,
+  });
+  const { ref: ref7, inView: inView7 } = useInView({
+    triggerOnce: false,
+    threshold: 0.5,
+  });
+  const { ref: ref8, inView: inView8 } = useInView({
+    triggerOnce: false,
+    threshold: 0.5,
+  });
   return (
     <div className="menu-container bg-primary">
       <motion.img 
@@ -27,6 +55,134 @@ const Menu = () => {
       />
       <section className="hero-section bg-primary py-[70px] px-[80px] gap-[28px]">
         <motion.div
+                ref={ref1}
+                initial={{ opacity: 0, y: -200 }}
+                animate={{
+                    opacity: inView1 ? 1 : 0,
+                    y: inView1 ? 0 : -200,
+                }}
+                transition={{ duration: 0.5 }}
+                className='flex justify-center items-center px-[74px]'>
+                <div className='relative flex flex-col items-center'>
+                 <p className='text-accent font-[600] text-[70px] leading-[84px] font-mont text-center'>Suya</p>
+                 <img src={vector} alt="Decorative Vector" className="vector-image w-[250px] mt-4" />
+                </div>
+        </motion.div>
+        <div ref={ref8} className='flex items-center gap-[35px] pt-[50px]'>
+          <motion.img 
+           src={frame120} 
+           alt="Frame 120" 
+           className='w-[720px] h-[695px]'
+           initial={{ x: -100, opacity: 0 }}
+           animate={{
+             x: inView8 ? 0 : -100,
+             opacity: inView8 ? 1 : 0
+           }}
+           transition={{
+             type: "spring",
+             bounce: 0.4,
+             duration: 1
+           }}
+          />
+          <motion.div 
+           className='text font-mont'
+           initial={{ x: 100, opacity: 0 }}
+           animate={{
+             x: inView8 ? 0 : 100,
+             opacity: inView8 ? 1 : 0
+          }}
+           transition={{
+             type: "spring",
+             bounce: 0.4,
+             duration: 1
+            }}
+           >
+            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
+              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
+              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
+              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$20,00</p>
+            </span>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
+              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Chillin&apos; Chicken Suya</h2>
+              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken garnished with onion & cucumber.</h3>
+              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$30,00</p>
+            </span>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
+              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Wingin&apos; it Suya</h2>
+              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken wings garnished with onion & cucumber.</h3>
+              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
+            </span>
+            <button className='bg-accent rounded-sm text-primary w-[266px] h-[74px] p-[10px] gap-[10px] mt-4'>Order Now</button>
+            </motion.div>
+        </div>
+      </section>
+
+      <section className="hero-section bg-primary py-[70px] px-[80px] gap-[28px]">
+        <motion.div
+                ref={ref2}
+                initial={{ opacity: 0, y: -200 }}
+                animate={{
+                    opacity: inView2 ? 1 : 0,
+                    y: inView2 ? 0 : -200,
+                }}
+                transition={{ duration: 0.5 }}
+                className='flex justify-center items-center px-[74px]'>
+                <div className='relative flex flex-col items-center'>
+                 <p className='text-accent font-[600] text-[70px] leading-[84px] font-mont text-center'>Groovy Green Salad</p>
+                 <img src={vector} alt="Decorative Vector" className="vector-image w-[250px] mt-4" />
+                </div>
+        </motion.div>
+        <div ref={ref5} className='flex items-center gap-[35px] pt-[50px]'>
+          <motion.img 
+           src={salad} 
+           alt="Frame 120" 
+           className='w-[720px] h-[695px]'
+           initial={{ x: -100, opacity: 0 }}
+           animate={{
+             x: inView5 ? 0 : -100,
+             opacity: inView5 ? 1 : 0
+           }}
+           transition={{
+             type: "spring",
+             bounce: 0.4,
+             duration: 1
+           }}
+          />
+          <motion.div 
+           className='text font-mont'
+           initial={{ x: 100, opacity: 0 }}
+           animate={{
+             x: inView5 ? 0 : 100,
+             opacity: inView5 ? 1 : 0
+          }}
+           transition={{
+             type: "spring",
+             bounce: 0.4,
+             duration: 1
+            }}
+           >
+            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
+              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
+              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
+              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$20,00</p>
+            </span>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
+              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Chillin&apos; Chicken Suya</h2>
+              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken garnished with onion & cucumber.</h3>
+              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$30,00</p>
+            </span>
+            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
+              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Wingin&apos; it Suya</h2>
+              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken wings garnished with onion & cucumber.</h3>
+              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
+            </span>
+            <button className='bg-accent rounded-sm text-primary w-[266px] h-[74px] p-[10px] gap-[10px] mt-4'>Order Now</button>
+            </motion.div>
+        </div>
+        
+      </section>
+      <section className="hero-section bg-primary py-[70px] px-[80px] gap-[28px]">
+      <motion.div
                 ref={ref3}
                 initial={{ opacity: 0, y: -200 }}
                 animate={{
@@ -35,14 +191,40 @@ const Menu = () => {
                 }}
                 transition={{ duration: 0.5 }}
                 className='flex justify-center items-center px-[74px]'>
-                <div className='relative flex flex-col items-start'>
-                 <p className='text-accent text-xl font-[600] text-[50px] leading-[84px] font-mont align-middle'>Groovy Green Salad</p>
-                 <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
+                <div className='relative flex flex-col items-center'>
+                 <p className='text-accent font-[600] text-[70px] leading-[84px] font-mont text-center'>Suya Mix Burger</p>
+                 <img src={vector} alt="Decorative Vector" className="vector-image w-[250px] mt-4" />
                 </div>
         </motion.div>
-        <div className='flex items-center gap-[35px] pt-[50px]'>
-           <img src={frame120} alt="Frame 120" className='w-[720px] h-[695px]'/>
-           <div className='text font-mont'>
+        <div ref={ref6} className='flex items-center gap-[35px] pt-[50px]'>
+          <motion.img 
+           src={burger} 
+           alt="Frame 120" 
+           className='w-[720px] h-[695px]'
+           initial={{ x: -100, opacity: 0 }}
+           animate={{
+             x: inView6 ? 0 : -100,
+             opacity: inView6 ? 1 : 0
+           }}
+           transition={{
+             type: "spring",
+             bounce: 0.4,
+             duration: 1
+           }}
+          />
+          <motion.div 
+           className='text font-mont'
+           initial={{ x: 100, opacity: 0 }}
+           animate={{
+             x: inView6 ? 0 : 100,
+             opacity: inView6 ? 1 : 0
+          }}
+           transition={{
+             type: "spring",
+             bounce: 0.4,
+             duration: 1
+            }}
+           >
             <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
@@ -59,18 +241,54 @@ const Menu = () => {
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
             </span>
             <button className='bg-accent rounded-sm text-primary w-[266px] h-[74px] p-[10px] gap-[10px] mt-4'>Order Now</button>
-            </div>
+            </motion.div>
         </div>
         
       </section>
       <section className="hero-section bg-primary py-[70px] px-[80px] gap-[28px]">
-        <div className='flex flex-col items-center justify-center gap-[10px]'>
-          <p className='text-accent text-xl font-[600] text-[50px] leading-[84px] font-mont align-middle'>Groovy Green Salad</p>
-          <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
-        </div>
-        <div className='flex items-center gap-[35px] pt-[50px]'>
-           <img src={salad} alt="Frame 120" className='w-[720px] h-[695px]'/>
-           <div className='text font-mont'>
+      <motion.div
+                ref={ref4}
+                initial={{ opacity: 0, y: -200 }}
+                animate={{
+                    opacity: inView4 ? 1 : 0,
+                    y: inView4 ? 0 : -200,
+                }}
+                transition={{ duration: 0.5 }}
+                className='flex justify-center items-center px-[74px]'>
+                <div className='relative flex flex-col items-center'>
+                 <p className='text-accent font-[600] text-[70px] leading-[84px] font-mont text-center'>Drinks</p>
+                 <img src={vector} alt="Decorative Vector" className="vector-image w-[250px] mt-4" />
+                </div>
+        </motion.div>
+        <div ref={ref7} className='flex items-center gap-[35px] pt-[50px]'>
+          <motion.img 
+           src={burger} 
+           alt="Frame 120" 
+           className='w-[720px] h-[695px]'
+           initial={{ x: -100, opacity: 0 }}
+           animate={{
+             x: inView7 ? 0 : -100,
+             opacity: inView7 ? 1 : 0
+           }}
+           transition={{
+             type: "spring",
+             bounce: 0.4,
+             duration: 1
+           }}
+          />
+          <motion.div 
+           className='text font-mont'
+           initial={{ x: 100, opacity: 0 }}
+           animate={{
+             x: inView7 ? 0 : 100,
+             opacity: inView7 ? 1 : 0
+          }}
+           transition={{
+             type: "spring",
+             bounce: 0.4,
+             duration: 1
+            }}
+           >
             <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
               <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
               <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
@@ -87,65 +305,8 @@ const Menu = () => {
               <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
             </span>
             <button className='bg-accent rounded-sm text-primary w-[266px] h-[74px] p-[10px] gap-[10px] mt-4'>Order Now</button>
-            </div>
+            </motion.div>
         </div>
-        
-      </section>
-      <section className="hero-section bg-primary py-[70px] px-[80px] gap-[28px]">
-        <div className='flex flex-col items-center justify-center gap-[10px]'>
-          <p className='text-accent text-xl font-[600] text-[50px] leading-[84px] font-mont align-middle'>Suya Mix Burger</p>
-          <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
-        </div>
-        <div className='flex items-center gap-[35px] pt-[50px]'>
-           <img src={burger} alt="Frame 120" className='w-[720px] h-[695px]'/>
-           <div className='text font-mont'>
-            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
-              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
-              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
-              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$20,00</p>
-            </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
-              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Chillin&apos; Chicken Suya</h2>
-              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken garnished with onion & cucumber.</h3>
-              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$30,00</p>
-            </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
-              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Wingin&apos; it Suya</h2>
-              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken wings garnished with onion & cucumber.</h3>
-              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
-            </span>
-            <button className='bg-accent rounded-sm text-primary w-[266px] h-[74px] p-[10px] gap-[10px] mt-4'>Order Now</button>
-            </div>
-        </div>
-        
-      </section>
-      <section className="hero-section bg-primary py-[70px] px-[80px] gap-[28px]">
-        <div className='flex flex-col items-center justify-center gap-[10px]'>
-          <p className='text-accent text-xl font-[600] text-[50px] leading-[84px] font-mont align-middle'>Drinks</p>
-          <img src={vector} alt="Decorative Vector" className="vector-image w-[250px]" />
-        </div>
-        <div className='flex items-center gap-[35px] pt-[50px]'>
-           <img src={burger} alt="Frame 120" className='w-[720px] h-[695px]'/>
-           <div className='text font-mont'>
-            <span className='gap-[11px] pb-4 flex flex-col border-b-2 border-dashed border-orange'>
-              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>BOSS! Beef Suya</h2>
-              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled beef garnished with onion & cucumber.</h3>
-              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$20,00</p>
-            </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
-              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Chillin&apos; Chicken Suya</h2>
-              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken garnished with onion & cucumber.</h3>
-              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$30,00</p>
-            </span>
-            <span className='gap-[11px] py-4 flex flex-col border-b-2 border-dashed border-orange'>
-              <h2 className='text-accent text-[40px] leading-[48px] font-[600]'>Wingin&apos; it Suya</h2>
-              <h3 className='text-white font-[500] text-[20px] leading-[37.5px]'>Mild / Spicy Grilled chicken wings garnished with onion & cucumber.</h3>
-              <p className='text-accent font-[600] text-[18px] leading-[27px]'>$17,00</p>
-            </span>
-            <button className='bg-accent rounded-sm text-primary w-[266px] h-[74px] p-[10px] gap-[10px] mt-4'>Order Now</button>
-            </div>
-        </div>
-        
       </section>
 
     </div>
