232d7cec75a963707948456fec79cb0bdf622aac
Rheno001 <renfredreuben@gmail.com>
2024-12-24 19:47:33 +0100

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,8 @@
 
 const Card = ({img, heading, para}) => {
     return ( <>
-    <div className='flex flex-col justify-center items-center w-[260px] lg:w-[412px]'>
-        <img src={img} alt="" className='w-full object-cover relative z-[20] rounded-b-none' />
+    <div className='flex flex-col justify-center items-center w-[260px] sm:w-[412px]'>
+        <img src={img} alt="" className='w-[100%] object-cover relative z-[20] rounded-b-none' />
         <div className='bg-accent w-[260px]  sm:w-[412px] min-h-[100px] lg:min-h-[150px] rounded-b-[20px] px-[20px] pb-[20px]'>
                 <h1 className='font-[700] lg:text-[32px] lg:leading-[41px] text-primary'>{heading}</h1>
                 <p className='font-[500] text-[14px] lg:text-[16px] lg:leading-[24px]'>{para}</p>
